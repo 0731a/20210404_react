856f7b28869905ff2aa91d19120983834423c814
0731a <72274498+0731a@users.noreply.github.com>
2021-04-12 04:35:42 +0900

    [FIX] 부모 컴포넌트 추가 및 props를 이용하여 부모와 통신

--- src/index.js
+++ src/index.js
@@ -3,25 +3,31 @@ import ReactDOM from 'react-dom';
 
 class Card extends React.Component {
 	render() {
-		const img = "img/strings.png";
-		const imgalt = "string";
-		const desc = "A very authentic and beautiful instrument !";
-		const price = 100;
-		const productname = "Strings";
 		return (
 			<div className="col-md-6 col-lg-4 d-flex align-items-stretch">
 				<div className="card mb-3">
-						<img className="card-img-top" src={img} alt={imgalt} />
+						<img className="card-img-top" src={this.props.img} alt={this.props.imgalt} />
 						<div className="card-body">
-							<h4 className="card-title">{productname}</h4>
-								price: <strong>{price}</strong>
-							<p className="card-text">{desc}</p>
+							<h4 className="card-title">{this.props.productname}</h4>
+								price: <strong>{this.props.price}</strong>
+							<p className="card-text">{this.props.desc}</p>
 							<a href="#" className="btn btn-primary">Buy</a>
 						</div>
 					</div>
 				</div>
 			);
 		}
+}
+
+class CardContainer extends React.Component{
+	render(){
+		return(
+			<div>
+				<Card key='1' img="img/strings.png" alt = "strings" productName = "Strings" price = '100.0' desc = "A very authentic and beautiful instrument !" />
+				<Card key='2' img="img/redGuitar.jpeg" alt = "redGuitar" productName = "RedGuitar" price = '299.0' desc = "A  really cool red guitar that can produce super cool music!" />
+			</div>
+		);
+	}
 }
 
 
