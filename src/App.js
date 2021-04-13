7a25e82caa17fe64a418a9bd2716e1c8ed3225b4
0731a <72274498+0731a@users.noreply.github.com>
2021-04-14 05:34:28 +0900

    [ADD] nav-router의 연결

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CardContainer from './ProductCards';
+import Nav from './Navigation';
+import { SignInModalWindow, BuyModalWindow } from '/.modalwindows';
+import About from './About';
+import Orders from './orders';
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user:{
+        loggedin: false,
+        name: ""
+      }
+    };
+  }
+    
+  render(){
+    return (
+      <div >
+        <Router>
+          <div>
+            <Nav user={this.state.user} />
+            <div className="container pt-4 mt-4">
+              <Router exact path="/" render={()=> <CardContainer location="cards.json"/>} />
+              <Router path="/promos" render={()=> <CardContainer location="promos.json" promo={true}/>} />
+              { this.state.user.loggedin? <Router path="/myorders" render={()=> <Orders/>} />: null }
+              <Router path="/about" Component={About}/>
+            </div>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
