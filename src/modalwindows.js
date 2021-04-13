ede62cb059608ea5f21682811baa1782f48a321c
0731a <72274498+0731a@users.noreply.github.com>
2021-04-13 19:34:29 +0900

    [ADD] BuyModalwindow 클래스형 컴포넌트-> 함수형 컴포넌트

--- src/modalwindows.js
+++ src/modalwindows.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Modal, ModalHeader, ModalBody} from 'reactstrap'; 
 
-export class BuyModalWindow extends React.Component{
-    render(){
-        return(
-            <modal id="buy" tabIndex="-1" role="dialog" isOpen={props.showModal} toggle={props.togle}>
-                <div role="document">
-                    <ModalHeader toggle={props.toggle} className="bg-success text-white">
-                        Buy Item
-                    </ModalHeader>
-                    <ModalBody>
-                      {/*신용카드 결제 폼*/}
-                    </ModalBody>
-                </div>
-            </modal>
-        );
-    }
+export function BuyModalWindow (props){
+    
+    return(
+        <modal id="buy" tabIndex="-1" role="dialog" isOpen={props.showModal} toggle={props.togle}>
+            <div role="document">
+                <ModalHeader toggle={props.toggle} className="bg-success text-white">
+                    Buy Item
+                </ModalHeader>
+                <ModalBody>
+                    {/*신용카드 결제 폼*/}
+                </ModalBody>
+            </div>
+        </modal>
+    );
+    
 }
