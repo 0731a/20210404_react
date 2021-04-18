d6f571c278f91bd9853cccfcd90fa9a35439aa5b
0731a <72274498+0731a@users.noreply.github.com>
2021-04-18 21:06:40 +0900

    [FIX] gorm연결을 위한 설정 추가

--- server_go/src/models/models.go
+++ server_go/src/models/models.go
@@ -1,26 +1,34 @@
 package models
 
-type Product struct{
-	Image string `json:"img"`
-	ImagAlt string `json:"imgalt"`
-	Price float64 `json:"price"`
-	Promotion float64 `json:"promotion"`
-	ProductName string `json:"productname"`
-	Description string `json:"desc`
+import "time"
+
+type Product struct {
+	gorm.Model
+	Image       string  `json:"img"`
+	ImagAlt     string  `json:"imgalt" gorm:"column:imgalt"`
+	Price       float64 `json:"price"`
+	Promotion   float64 `json:"promotion"` //sql.NullFloat64
+	ProductName string  `json:"productname" gorm:"column:productname"`
+	Description string  `json:"desc"`
 }
 
-type Customer struct{
-	FirstName string `json:firstname`
-	LastName string `json:"lastname"`
-	Email string `json:"email"`
-	LoggedIn bool `json:"loggedin"`
+type Customer struct {
+	gorm.Model
+	Name      string   `json:"name"`
+	FirstName string  `gorm:"column:firstname"` `json:"firstname"`
+	LastName  string  `gorm:"column:lastname"` `json:"lastname"`
+	Email     string  `gorm:"column:email"` `json:"email"`
+	LoggedIn  bool    `gorm:"column:loggedin"` `json:"loggedin"`
+	Pass      string  `json:"password"`
+	Orders    []Order `json:"orders"`
 }
 
-type Order struct{
+type Order struct {
+	gorm.Model
 	Product
 	Customer
-	CustomerID int `json:"customer_id"`
-	ProductID int `json:"product_id`
-	Price float64 `json:"sell_price`
-	PurchaseDate time.Time `json:"purchase_date`
+	CustomerID   int       `gorm:"column:customer_id" json:"customer_id"`
+	ProductID    int       `gorm:"column:product_id" json:"product_id`
+	Price        float64   `gorm:"column:price" json:"sell_price`
+	PurchaseDate time.Time `gorm:"column:purchase_date" json:"purchase_date`
 }
